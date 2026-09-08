5a59cffcbeed7d9f19c8f023f228e133e39f1bdf
4pqqvrsh2t-sudo <4pqqvrsh2t@privaterelay.appleid.com>
2026-09-07 22:38:51 -0400

    Bump service worker cache for accuracy hotfixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='roadlimit-2026-09-07-v3';
+const CACHE='roadlimit-2026-09-07-v4';
 const SHELL=['./','./index.html','./styles.css','./core.js','./sources.js','./matcher.js','./review.js','./storage.js','./esp32-bridge.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
